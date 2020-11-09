6b2a6e306d7f7e994c792c8b7be13a45f15cd54c
edewa <joshedewa@gmail.com>
2020-11-09 15:12:38 +0300

    sorted some issues

--- photos/tasks.py
+++ photos/tasks.py
@@ -1,16 +1,11 @@
-from celery.schedules import crontab
-from celery.decorators import periodic_task
+from celery.decorators import task
 from celery.utils.log import get_task_logger
 
 from photos.utils import save_latest_flickr_image
 
 logger = get_task_logger(__name__)
 
-@periodic_task(
-    run_very=(crontab(minute='*/15')),
-    name='task_save_latest_flickr_image',
-    ignore_result=True
-)
+@task(name="task_save_latest_flickr_image")
 def task_save_latest_flickr_image():
     """
     Saves latest image from Flickr
--- picha/celery.py
+++ picha/celery.py
@@ -1,6 +1,10 @@
+# from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
+from celery.schedules import crontab
+# from photos import tasks
+
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'picha.settings')
@@ -13,8 +17,16 @@ app = Celery('picha')
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+app.conf.beat_schedule = {
+    "task-save-latest-flickr-image": {
+        "task": "task_save_latest_flickr_image",
+        "schedule": crontab(minute='*/10'),
+        "args": (),
+    }
+}
+
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks
+app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
